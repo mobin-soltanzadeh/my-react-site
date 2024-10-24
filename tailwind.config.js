daf286f23affaf0366485c9601566f05d2e15be6
mobin <mobin.soltanzadeh.work@gmail.com>
2024-10-24 11:13:21 +0330

    install Tailwindcss and add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.js"],
+    // content: ["./{src, public}/*.{js, html}"],
+    darkMode: "class",
+    theme: {
+        screens: {
+        "sm": "640px",
+        "md": "768px",
+        "base": "1024px",
+        "lg": "1174px",
+        "xl": "1350px",
+        "2xl": "1536px",
+        },
+        extend: {
+        fontFamily: {
+            "adlery-a": "Adlery-a",
+            "adlery-b": "Adlery-b",
+            "adlery-c": "Adlery-c",
+            "mangiola": "Mangiola",
+            "vazir-extraLight" : "Vazir1",
+            "vazir-light" : "Vazir2",
+            "vazir-medium" : "Vazir3",
+            "shabnam" : "Shabnam1",
+            "shabnam-light" : "Shabnam2",
+            "shabnam-medium" : "Shabnam3",
+            "shabnam-thin" : "Shabnam4",
+        }
+        },
+    },
+    plugins: [
+        function ({ addVariant }) {
+            addVariant('child', '& > *');
+            addVariant('child-hover', '& > *:hover');
+        }
+    ],
+}
